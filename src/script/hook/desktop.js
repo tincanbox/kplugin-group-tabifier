41494c61b74cf32012550a673eb311cda9a83ef0
matooy <matsui@psersonalagent.co.jp>
2019-10-18 18:45:18 +0900

    f

--- src/script/hook/desktop.js
+++ src/script/hook/desktop.js
@@ -16,7 +16,7 @@ import '../../asset/scss/desktop.scss';
     selector: {
       link_group_class: 'kplugin-group-tabifier-group-anchor',
       target_wrapper: '#record-gaia',
-      target_cont: '.gaia-argoui-app-toolbar',
+      target_cont: null,
       target_list: '.control-group-gaia',
       target_label: '.group-label-gaia',
       exclude_offset: 0
@@ -39,18 +39,43 @@ import '../../asset/scss/desktop.scss';
   var p = K.init();
 
   K.$k.events.on('app.record.detail.show', function(e){
-    if(!S.inited){
-      p.then(init);
-      S.inited = true;
-    }
+    find_wrapper().remove();
+    C.selector.target_cont = '.gaia-argoui-app-show-toolbar';
+    p.then(init);
+    return e;
   });
+
+  K.$k.events.on('app.record.edit.show', function(e){
+    find_wrapper().remove();
+    C.selector.target_cont = '.gaia-argoui-app-edit-buttons-contents';
+    p.then(init);
+    return e;
+  });
+  K.$k.events.on('app.record.create.show', function(e){
+    find_wrapper().remove();
+    C.selector.target_cont = '.gaia-argoui-app-edit-form';
+    p.then(init);
+    return e;
+  });
+
+  function find_wrapper(){
+    return $('#' + C.wrapper_id);
+  }
+
+  function is_available(){
+    return find_wrapper().length > 0;
+  }
 
   function init(){
     C.wrapper = $('<section>');
 
     update_info();
     build({
-      position: 'after'
+      position: 'append'
+    });
+
+    $(document).on('click', C.selector.target_list, function(e){
+      update_info();
     });
 
     $(window).on('scroll', function(){
@@ -68,6 +93,7 @@ import '../../asset/scss/desktop.scss';
       }
 
       update_anchor_class(btwn);
+
     });
   }
 
@@ -90,8 +116,6 @@ import '../../asset/scss/desktop.scss';
       w.append(build_anchor(data[k]));
     }
 
-    S.built = true;
-
     t[o.position](w);
     return t;
   }
@@ -102,7 +126,7 @@ import '../../asset/scss/desktop.scss';
     var idx = parseInt($el.attr(C.attr_data_index));
     var tpx = 0;
     if(idx == -1){
-      tpx = C.wait_scrolltop;
+      tpx = calc_border();
     }else{
       var dst;
       for(var k in S.info){
@@ -127,16 +151,42 @@ import '../../asset/scss/desktop.scss';
   }
 
   function calc_border(info){
-    return info.position - C.wait_scrolltop + C.offset_keep;
+    if(info){
+      return info.position - C.wait_scrolltop + C.offset_keep;
+    }else{
+      return C.initial_offset - C.wait_scrolltop + C.offset_keep;
+    }
+  }
+
+  function fetch_height(sel){
+    if(sel instanceof Array){
+      return sel.reduce(function(m, s){
+        var h = fetch_height(s);
+        //console.log(s, $(s).get(0), h);
+        return m + h;
+      }, 0);
+    }else{
+      var $el = $(sel);
+      if($el.length){
+        return $el.height();
+      }else{
+        return 0;
+      }
+    }
   }
 
   function update_info(){
     console.log("update info");
+    C.initial_offset = $(C.selector.target_wrapper).offset().top + 18;
+
     C.wait_scrolltop
-      = $('.gaia-argoui-app-show-toolbar').height()
-      + $('.gaia-header-toolbar-navigation').height()
-      + $('.gaia-argoui-app-toolbar').height()
-    ;
+      = fetch_height([
+        // common
+        '.gaia-header-toolbar-navigation',
+        '.gaia-argoui-app-titlebar',
+        '.gaia-argoui-app-infobar-breadcrumb-iconlist',
+      ]) + 80;
+
     C.offset_keep
       = 0
       + C.wrapper.height()
@@ -148,8 +198,8 @@ import '../../asset/scss/desktop.scss';
       var o = new DestInfo($(this), i);
       S.info[o.name] = o;
     });
-    console.log("C", C);
-    console.log("S", S);
+    //console.log("C", C);
+    //console.log("S", S);
     return S.info;
   }
 
